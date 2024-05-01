f352c32076272cdf5eae96b4fba931fca45662bf
Avitalys <avitalyo52@gmail.com>
2024-05-01 19:57:10 +0300

    Added a static Top 'n Bottom obstacle pair

--- src/entities/index.js
+++ src/entities/index.js
@@ -2,6 +2,8 @@ import Matter from 'matter-js';
 import Bird from '../components/Bird';
 import Floor from '../components/Floor';
 import { Dimensions } from 'react-native';
+import Obstacle from '../components/Obstacle';
+import { getPipeSizePosPair } from '../utils/random';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
@@ -11,11 +13,27 @@ export default (restart) => {
 
   let world = engine.world;
 
-  world.gravity.y = 0.3;
+  world.gravity.y = 0.9;
+
+  const PipePosSize1 = getPipeSizePosPair(-300);
 
   return {
     physics: { engine, world },
     Bird: Bird(world, 'green', { x: 50, y: 200 }, { height: 40, width: 40 }),
+    ObstacleTop1: Obstacle(
+      world,
+      'ObstacleTop1',
+      'red',
+      PipePosSize1.pipeTop.pos,
+      PipePosSize1.pipeTop.size,
+    ),
+    ObstacleBottom1: Obstacle(
+      world,
+      'ObstacleBottom1',
+      'blue',
+      PipePosSize1.pipeBottom.pos,
+      PipePosSize1.pipeBottom.size,
+    ),
     Floor: Floor(
       world,
       'green',
